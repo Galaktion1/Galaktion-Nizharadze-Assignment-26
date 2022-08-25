32416763448607b0d07ff7dcf9b92f17ddde6d83
Gaga <galaqtionn@gmail.com>
2022-08-25 13:44:55 +0400

    Initial Commit

---
+++ Galaktion Nizharadze, Assignment #26/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Galaktion Nizharadze, Assignment #26
+//
+//  Created by Gaga Nizharadze on 25.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
